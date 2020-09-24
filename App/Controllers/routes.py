5e9ef54a045d16489e1259162ef1677dc2fc4834
Yoharu <jonathas_a.a@hotmail.com>
2020-09-24 01:50:52 -0300

    feita as páginas de cadastro de produtor e crud e usuários

--- App/Controllers/routes.py
+++ App/Controllers/routes.py
@@ -4,6 +4,8 @@ from markupsafe import escape
 from App import app,db
 from App.Templates.Components.formLogin import LoginForm
 from App.Templates.Components.formRegister import RegisterForm
+from App.Templates.Components.formProducer import ProducerForm
+
 import json
 import os,os.path
 from App.Models.Models import  User,Producer
@@ -14,18 +16,13 @@ from App.Controllers.loginController import AuthFinger,Register
 # @app.route("/", defaults = {"user": None})
 # def index(user):
 #     return render_template('index.html',user = user)
-class Usuario:
-    def __init__(self,name , username, nivel):
-        self.name = name
-        self.username = username
-        self.nivel = nivel
 
 @app.route("/")
 def index():
     if 'user' in session:
         Producers = Producer.query.all()
         return render_template('index.html',
-                user = escape(session['user']),
+                nivel = int(escape(session['nivel'])),
                 Producers = Producers)
     return redirect(url_for('login'))
 
@@ -44,8 +41,10 @@ def login():
 
             # validando digital
             if AuthFinger(userSelect.fingerimage):
-                currentUser = Usuario(userSelect.name, userSelect.username, userSelect.nivelAcesso)
-                session['user'] = currentUser
+
+                # validou com sucesso
+                session['user'] = userSelect.username
+                session['nivel'] = userSelect.nivelAcesso
                 return redirect(url_for('index'), code=302)
                 
             errorFinger = "Digital não cadastrada ou incorreta"
@@ -62,10 +61,25 @@ def login():
 def logout():
     # remove the username from the session if it's there
     session.pop('user', None)
+    session.pop('nivel',None)
     return redirect(url_for('login'))
 
-# register route
-@app.route("/register", methods =['POST','GET'])
+
+@app.route("/users")
+def indexUser():
+    users = User.query.all()
+    print(users)
+    return render_template('user/index.html',users= users)
+
+@app.route("/users/<id>")
+def user(id):
+    form = RegisterForm()
+    user = User.query.get(id)
+    print(user)
+    return render_template('user/show.html',user = user,form=form)
+
+# user create route
+@app.route("/users/create", methods =['POST','GET'])
 def register():
     form = RegisterForm()
     userExist = User.query.filter_by(username=form.username.data).first()
@@ -79,11 +93,19 @@ def register():
         db.session.add(createUser)
         db.session.commit()
         form= RegisterForm()
-        return render_template('register.html', form = form, messageBd="Cadastrado com sucesso" ,user = escape(session['username']))
+        return render_template('user/create.html', form = form, messageBd="Cadastrado com sucesso")
 
-    return render_template('register.html', form = form, user = escape(session['username']))
+    return render_template('user/create.html', form = form, user = escape(session['username']))
 
-
+@app.route("/produtores/create", methods =['POST','GET'])
+def createProducer():
+    form = ProducerForm()
+    if form.validate_on_submit():
+        createProducer = Producer(form.name.data, form.farming.data, form.annualamount.data, form.pesticide.data)
+        db.session.add(createProducer)
+        db.session.commit()
+        return render_template("createProducer.html",form=form,messageBd="Cadastrado com sucesso")
+    return render_template("createProducer.html",form = form)
 
 
 @app.route("/create")
@@ -103,7 +125,6 @@ def select():
 def update():
     u = User.query.filter_by(username="jonathas").first()
     u.username="jonathasUPDATE"
-    print(u.username)
     db.session.add(u)
     db.session.commit()
     return "UPDATEE"
@@ -114,3 +135,27 @@ def delete():
     db.session.delete(d)
     db.session.commit()
     return "DELETADO"
+
+@app.route("/deleteuser",methods =['POST','GET'])
+def deleteuser():
+    promisse = json.loads(request.data)
+    d = User.query.get(promisse['id'])
+    db.session.delete(d)
+    db.session.commit()
+    return "DELETADO"
+
+@app.route("/updateuser",methods =['POST','GET'])
+def teste():
+    promisse = json.loads(request.data)
+    
+    u = User.query.get(promisse['id'])
+
+    u.username=promisse['username']
+    u.name=promisse['nome']
+    u.email=promisse['email']
+    u.nivelAcesso=promisse['nivel']
+    db.session.add(u)
+    db.session.commit()
+
+
+    return request.values
