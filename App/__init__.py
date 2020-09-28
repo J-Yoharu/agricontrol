4a3589baee9792a0d4262a5bc4f8386632512456
Yoharu <jonathas_a.a@hotmail.com>
2020-09-28 03:19:03 -0300

    add config template

--- App/__init__.py
+++ App/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 # storage for tag img just work in static folder 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='Templates')
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
