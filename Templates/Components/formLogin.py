65a9f544166739f50141dc61dfeea3bac481af31
Yoharu <jonathas_a.a@hotmail.com>
2020-09-28 03:04:13 -0300

    add config template

--- Templates/Components/formLogin.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, FileField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    username = StringField("username", validators = [DataRequired()])
-    password = PasswordField("Senha")
-    image = FileField("image", validators = [DataRequired()])
